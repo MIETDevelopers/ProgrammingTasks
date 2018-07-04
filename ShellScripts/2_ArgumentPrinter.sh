2a34aa76e4d3391bc6eb42fe79a130568af37fdf
Rishi <rishi.cse@mietjammu.in>
2018-07-04 17:27:03 +0530

    Update

--- ShellScripts/2_ArgumentPrinter.sh
+++ ShellScripts/2_ArgumentPrinter.sh
@@ -5,4 +5,9 @@
 #Purpose
 
 #Add command here to print number of arguments passed from shell
+
 #Add command here to print arguments list
+
+#Add command here to pring firt argument only
+
+#Add command here to print script name
