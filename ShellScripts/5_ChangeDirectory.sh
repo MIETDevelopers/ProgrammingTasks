7ec7ca4cf13dca9a39e0f067cda160d0a21d9ba1
Rishi <rishi.cse@mietjammu.in>
2018-07-08 00:51:48 +0530

    Create 5_ChangeDirectory.sh

---
+++ ShellScripts/5_ChangeDirectory.sh
@@ -0,0 +1,14 @@
+#Author: Your_Name
+#Created on: Creation Date
+
+#Problem Statement:
+#The sample directory stucture on unix system is following
+#/check1/check2
+#/check3/check4
+#/check/4check
+
+#Current working directory is check2
+#How to change directory to 4check
+# - Using absolute path
+# - Using relative path
+# - Using relative path via check4
