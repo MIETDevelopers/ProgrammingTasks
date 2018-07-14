d01eacb86b82c6d591a9615fc06c1174e98b903c
Avinash Koshal <31934725+aaavinash85@users.noreply.github.com>
2018-07-14 12:53:54 +0530

    Add files via upload

---
+++ ShellScripts/Problems/2_ArgumentPrinter.sh
@@ -0,0 +1,13 @@
+#Add shebang line here
+
+#Author Name
+#Creation Date
+#Purpose
+
+#Add command here to print number of arguments passed from shell
+
+#Add command here to print arguments list
+
+#Add command here to pring firt argument only
+
+#Add command here to print script name
---
+++ ShellScripts/Problems/3_Looper.sh
@@ -0,0 +1,7 @@
+#Add shebang line here
+
+#Author Name
+#Creation Date
+#Purpose
+
+#Add logic here to print passed arguments 5 times using different kinds of loops in shell scripting
