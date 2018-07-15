865ab39b25fb2d7daa7b556a0c5495db08786a08
Avinash Koshal <31934725+aaavinash85@users.noreply.github.com>
2018-07-15 17:56:23 +0530

    Add files via upload

---
+++ CPrograms/Problems/08_Array.c
@@ -0,0 +1,26 @@
+/* Author
+   Date
+    what to do
+1) Declare a variable to store elements in an array and print it
+   Expected O/P: please enter the elements :
+                 the elements stored are:
+2) Define a function to find the sum of all elements in the array:
+   Expected O/P: the elements are 1,2,3,4,5
+                 sum= 15 
+3) Define a function to find the sum of all elements in the array:
+   Expected O/P: the elements are 1,2,3,4,5
+                 sum= 15
+4) Define a function to copy the elememts of one array into anothe
+   Expected o/p: the elements of first array = 1,2,3
+                 the elements of copied array = 1,2,3
+5) Define a function to create a 2D array
+
+6) Call a function to multiply all the elements of 1D array with a specefic number
+   Expected O/P: the elements of array are: 1,2,3
+                 the revised array is:2,4,6 
+7) Define a function to multiply the elements of the 2D array:
+
+
+ */
+
+
