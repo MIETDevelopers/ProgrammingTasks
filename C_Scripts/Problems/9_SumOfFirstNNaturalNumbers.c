df9c4cd4293cd2fe1d1332e0e3c0e40b3924a086
Avinash Koshal <31934725+aaavinash85@users.noreply.github.com>
2018-07-15 16:57:29 +0530

    Add files via upload

---
+++ C_Scripts/Problems/9_SumOfFirstNNaturalNumbers.c
@@ -0,0 +1,7 @@
+/* Author
+   Date
+      What to do
+ Define a function to find the sum of first 10 natural numbers.
+Expected Output :The first 10 natural number is 1 2 3 4 5 6 7 8 9 10
+The Sum is : 55
+*/
